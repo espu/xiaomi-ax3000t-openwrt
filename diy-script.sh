4815f85f7c86afe3551e8b8d64f33e30d97d5367
espu <183891227+espu@users.noreply.github.com>
2025-06-06 03:10:39 +0800

    Update and rename diy-part1.sh to diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -18,6 +18,8 @@ rm -rf feeds/luci/themes/luci-theme-netgear
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
+# DIY rm
+rm -rf feeds/luci/applications/luci-app-ssr-plus
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
