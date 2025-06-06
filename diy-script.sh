d83ae6ee4fe7a798314b413ed070032c73d84ba8
espu <183891227+espu@users.noreply.github.com>
2025-06-06 12:30:32 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -91,9 +91,6 @@ sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.
 git pull
 
 ./scripts/feeds update -a
-
-sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/luci2/bin/config_generate
-
 ./scripts/feeds install -a
 
 make menuconfig
