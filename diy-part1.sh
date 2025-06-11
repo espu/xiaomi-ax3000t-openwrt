f8064801521791e31c898697552f60f3218a17d9
espu <efo@tuta.io>
2025-06-11 21:53:32 +0800

    up

--- diy-part1.sh
+++ diy-part1.sh
@@ -33,8 +33,6 @@ function git_sparse_clone() {
   mv -f $@ ../package
   cd .. && rm -rf $repodir
 }
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
@@ -46,16 +44,15 @@ sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.
 # 添加额外插件
 
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-
-#Add a Openclash feed depencies
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+#Add a Openclash feed source
+# git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # Add a passwall feed source
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
 echo 'src-git passwall_package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 
 # iStore
-# git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-# git_sparse_clone main https://github.com/linkease/istore luci
+git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+git_sparse_clone main https://github.com/linkease/istore luci
 
 
 
